0e81e5b7fa4a3caf29139a6bda08a1f4190eba20
mikeydgithub <ampgzo2@gmail.com>
2022-03-12 14:18:10 -0500

    database calls

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-const express = require('express');
-const express = require('express');
 
+// Connect to the MySQL Database
+const mysql = require('mysql2');
+
+const express = require('express');
 
 // PORT designation
 const PORT = process.env.PORT || 3001;
@@ -10,13 +12,23 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Test the Express.js connection
-// Create a GET test route.
-// app.get('/', (req, res) => {
-//     res.json({
-//         message: 'Hello World'
-//     });
-// });
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        // Your MySQL password
+        password: 'slack',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+// Using the query() method. This method run the SQL query and executes the callback with rows.
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+})
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
